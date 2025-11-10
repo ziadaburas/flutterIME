0084c228354bd0079309f62a53aaa72f293c6608
ZiadAburas <ziad2018zzzz@gmail.com>
2025-11-10 21:06:04 +0300

    full kotlin and flutter

---
+++ android/app/src/main/kotlin/com/example/ime/funs/Fun.kt
@@ -0,0 +1,17 @@
+package com.example.ime.funs
+
+import android.content.Context
+import android.util.AttributeSet
+import android.graphics.Color
+import android.graphics.Typeface
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import com.example.ime.FlutterIME
+import com.example.ime.R
+import com.example.ime.views.Key
+
+open class Fun{
+    
+}
